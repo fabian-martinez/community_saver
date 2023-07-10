0a38121067799c8b0dc18ee665b559868229c11e
FMRonin <fabianemartinez@gmail.com>
2023-07-10 17:53:25 -0500

    change controller and service

--- src/routes/loans.ts
+++ src/routes/loans.ts
@@ -11,10 +11,10 @@ const loanController:LoanController = new LoanController(loanService)
 
 logger.debug(`Loan Controller Inicailizado ${loanController}`)
 
-router.get ('/',                loanController.getLoans);
+router.get ('/search',          loanController.getLoans);
 router.get ('/:id',             loanController.getLoan);
 router.post('/',                loanController.postNewLoan)
 router.put ('/:id/payment',     loanController.putPayment);
-router.put ('/:id/disbursement', loanController.putDisburtsement);
+router.put ('/:id/disbursement',loanController.putDisburtsement);
 
 export default router
