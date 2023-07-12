de6a519b56df52ef204008b3f9977a52b5e60d38
FMRonin <fabianemartinez@gmail.com>
2023-07-12 14:37:27 -0500

    organize LoanService

--- src/services/loanService.ts
+++ src/services/loanService.ts
@@ -16,57 +16,7 @@ class LoanService {
 
         return loan;
 
-        const disbursmentData = loan.get('loan_disbursements') as LoanDisbursement[]
-        
-        const paymentData = loan.get('loan_payments') as LoanPayment[]
-
-        const filteredLoan = {
-            id:loan.id,
-            borrower_id:loan.borrower_id,
-            original_amount:loan.original_amount,
-            updated_amount:loan.updated_amount,
-            monthly_payment_amount:loan.monthly_payment_amount,
-            interest_rate:loan.interest_rate,
-            loan_date:loan.loan_date,
-            loan_type:loan.loan_type
-        } as Loan
-
-        // const result = [];
-        let balance = 0;
-
-        const paymentsAndDisbursements = [...disbursmentData, ...paymentData]
-
-        // console.log(paymentsAndDisbursements);
-        
-        // for (let i = 0; i < paymentData.length; i++) {
-        //     const payment = paymentData[i];
-        //     const disbursement = disbursmentData.find((item) => item.date <= payment.date);
-            
-        //     const entry = {
-        //         number: i,
-        //         date: payment.date,
-        //         capital: payment.payment_amount,
-        //         interest: payment.interest_amount,
-        //         balance: balance,
-        //         disbursement: disbursement?.disbursement_amount,
-        //         state: 'PAID',
-        //     };
-            
-        //     result.push(entry);
-            
-        //     balance += (disbursement?.disbursement_amount) ? disbursement?.disbursement_amount : 0;
-        //     balance -= payment.payment_amount;
-        // }
-        
-        throw new Error('Method not implemented.');
-
-        // return {loan:filteredLoan, payment_records:result}
     }
-
-    // public async getPaymentSchedule(loan_id: number):Promise<PaymentSchedule>  {
-        
-    //     // throw new Error('Method not implemented.');
-    // }
     
     public async getLoans(borrowerName?:string,loanType?:string):Promise<LoanModel[]> {
         
